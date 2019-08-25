d835c5bc64dd1dfeb708e3308e1502f2881fc729
NYAN CAT <NYANxCAT@pm.me>
2019-08-25 22:39:04 +0300

    Update Program.cs

--- njRAT C# Stub/Program.cs
+++ njRAT C# Stub/Program.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 
+
 namespace Lime
 {
 
+
     /*
      * │ Author       : NYAN CAT
-     * │ Name         : njRAT C# Stub
+     * │ Name         : njRAT C# Stub | Fixed for powershell
      * │ Contact      : https:github.com/NYAN-x-CAT
      * 
      * This program is distributed for educational purposes only.
      */
-
 
 
     public class Program
